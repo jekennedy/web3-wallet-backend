ac06535aa463e8cde8f57f6c4e52f426e7f3e06a
Justin Kennedy <justin@justinkennedy.ca>
2024-05-15 20:34:35 +0200

    config to support default funding amount for new wallets

--- src/config/blockchain.config.ts
+++ src/config/blockchain.config.ts
@@ -6,7 +6,6 @@ export interface BlockchainConfig {
   encryptionKey: string;
   funderPrivateKey: string;
   fundingAmountEth: string;
-  gasPriceGwei: string;
   gasLimit: number;
 }
 
@@ -14,5 +13,6 @@ export default registerAs(ConfigKeys.CONFIG_BLOCKCHAIN, () => ({
   rcpUrl: process.env.ETHEREUM_RPC_URL,
   encryptionKey: process.env.ENCRYPTION_KEY,
   funderPrivateKey: process.env.FUNDER_PRIVATE_KEY,
+  fundingAmountEth: process.env.FUNDING_AMOUNT_ETH,
   gasLimit: Number(process.env.GAS_LIMIT) || 21000,
 }));
